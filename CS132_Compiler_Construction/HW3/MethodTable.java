963641fe801a23295e1a8e39cb227777c5088159
Zhenhua Tan <52186042+wahjay@users.noreply.github.com>
2019-11-15 02:56:19 -0800

    Add files via upload

---
+++ CS132_Compiler_Construction/HW3/MethodTable.java
@@ -0,0 +1,165 @@
+import syntaxtree.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+
+
+class MethodTable {
+  HashMap<String, List<String>> methodTable;  //store object methods
+  HashMap<String, List<String>> fields;       //store object fields
+
+  //store local object name and its type
+  //tells me which method to call
+  HashMap<String, String> types;
+
+  private static MethodTable instance;
+  public static String cur_class;  //store current class scope
+  public static String new_class;  //used for object allocation
+  public static String cur_method; //store current method scope
+  public static String pre_method; //store previous method scope
+
+  private MethodTable() {
+    methodTable = new HashMap<>();
+    fields = new HashMap<>();
+    types = new HashMap<>();
+  }
+
+  public static MethodTable getInstance() {
+    if(instance == null)
+      instance = new MethodTable();
+
+    return instance;
+  }
+
+  public String getType(String n) {
+    return types.get(n);
+  }
+
+  public void insertTypes(String n, String t) {
+    types.put(n, t);
+  }
+
+  //get the object size
+  public int getOSize(String cn) {
+    List<String> fields_ = fields.get(cn);
+    return (fields_.size() + 1) * 4;
+  }
+
+
+  public int getFPosition(String cn, String fn) {
+    List<String> fields_ = fields.get(cn);
+
+    //if fields_ is null, we are probably in the main class
+    //or the field name is simply just a integer literal
+    if(fields_ == null)
+      return -1;
+
+    String field = cn + "." + fn;
+    int pos = fields_.indexOf(field);
+
+    // at this point, if pos equals -1, maybe because
+    // the field is inherited from its super class
+    // super class fields are stored in child class
+    // in this format: "superclassname.superclassmethod"
+    ////////////////////////////////////////
+    if(pos == -1) {
+      for(String entry : fields_) {
+        if(entry.substring(entry.indexOf(".")+1).equals(fn)) {
+          field = entry;
+          break;
+        }
+      }
+
+      //retrieve super class name and method name
+      //and then find its pos in the super class field
+      pos = field.indexOf(".");
+      String class_name = field.substring(0, pos);
+      String method_name = field.substring(pos+1);
+      fields_ = fields.get(class_name);
+      pos = fields_.indexOf(class_name + "." + method_name);
+    }
+    ////////////////////////////////////////
+
+    //if pos is still -1, return -1
+    if(pos == -1)
+      return -1;
+
+    return (pos + 1) * 4;
+  }
+
+  public int getMPosition(String cn, String mn) {
+    List<String> methods = methodTable.get(cn);
+    String method = cn + "." + mn;
+    int pos = methods.indexOf(method);
+
+
+    // at this point, if pos equals -1, maybe because
+    // the method is inherited from its super class
+    // super class methods are stored in child class
+    // in this format: "superclassname.superclassmethod"
+    ////////////////////////////////////////
+    if(pos == -1) {
+      for(String entry : methods) {
+        if(entry.substring(entry.indexOf(".")+1).equals(mn)) {
+          method = entry;
+          break;
+        }
+      }
+
+      //retrieve super class name and method name
+      //and then find its pos in the super class field
+      pos = method.indexOf(".");
+      String class_name = method.substring(0, pos);
+      String method_name = method.substring(pos+1);
+      methods = methodTable.get(class_name);
+      pos = methods.indexOf(class_name + "." + method_name);
+    }
+    ////////////////////////////////////////
+
+
+    //if pos == -1, maybe becaseu the wanted method
+    //is inherited from its parent class
+    if(pos == -1)
+      return -1;
+    return pos * 4;
+  }
+
+  public void insertFields(String cn, List<String> fn) {
+    fields.put(cn, fn);
+  }
+
+  public List<String> getFields(String cn) {
+    return fields.get(cn);
+  }
+
+  public void insertMethods(String cn, List<String> mn) {
+    methodTable.put(cn, mn);
+  }
+
+  public List<String> getMethods(String cn) {
+    return methodTable.get(cn);
+  }
+
+  public void print() {
+    for(Map.Entry<String, List<String>> entry : methodTable.entrySet()) {
+      List<String> methods = entry.getValue();
+      List<String> fields_ = fields.get(entry.getKey());
+      System.out.println("Class name: " + entry.getKey());
+      for(String field : fields_)
+        System.out.println("Field name: " + field);
+
+      for(String method : methods)
+        System.out.println("Method name: " + method);
+
+      System.out.println("_______________________________");
+    }
+  }
+
+  public void printTypes() {
+    for(Map.Entry<String, String> entry : types.entrySet()) {
+      System.out.println("Var Name: " + entry.getKey());
+      System.out.println("Type Name: " + entry.getValue());
+    }
+  }
+
+}
